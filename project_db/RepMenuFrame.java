d6ca96c78a24e582d1e4e06506f4eec1aaccd688
zilingg1208 <zilingg1208@gmail.com>
2026-05-04 22:56:58 -0400

    Add buttons for airline, aircraft, and airport management

--- project_db/RepMenuFrame.java
+++ project_db/RepMenuFrame.java
@@ -14,19 +14,62 @@ public class RepMenuFrame extends JFrame {
         JButton addFlight = new JButton("Add Flight");
         JButton deleteFlight = new JButton("Delete Flight");
         JButton waiting = new JButton("View Waiting List");
+        JButton logout = new JButton("Logout");
+
+        JButton addAirline = new JButton("Add Airline");
+        JButton deleteAirline = new JButton("Delete Airline");
+
+        JButton addAircraft = new JButton("Add Aircraft");
+        JButton editAircraft = new JButton("Edit Aircraft");
+        JButton deleteAircraft = new JButton("Delete Aircraft");
+
+        JButton addAirport = new JButton("Add Airport");
+        JButton editAirport = new JButton("Edit Airport");
+        JButton deleteAirport = new JButton("Delete Airport");
+
+        JButton editFlight = new JButton("Edit Flight");
 
         add(reserve);
-        add(edit);
-        add(addFlight);
-        add(deleteFlight);
         add(waiting);
+        add(edit);
+
+        add(addFlight);
+        add(editFlight);
+        add(deleteFlight);
+
+        add(addAirline);
+        add(deleteAirline);
+
+        add(addAircraft);
+        add(editAircraft);
+        add(deleteAircraft);
+
+        add(addAirport);
+        add(editAirport);
+        add(deleteAirport);
+
+        add(logout);
 
         reserve.addActionListener(e -> RepService.makeReservationForUser(new Scanner(System.in)));
         edit.addActionListener(e -> RepService.editReservation(new Scanner(System.in)));
         addFlight.addActionListener(e -> RepService.addFlight(new Scanner(System.in)));
         deleteFlight.addActionListener(e -> RepService.deleteFlight(new Scanner(System.in)));
         waiting.addActionListener(e -> RepService.viewWaitingList(new Scanner(System.in)));
+        addAircraft.addActionListener(e -> RepService.addAircraft(new Scanner(System.in)));
+        editAircraft.addActionListener(e -> RepService.editAircraft(new Scanner(System.in)));
+        deleteAircraft.addActionListener(e -> RepService.deleteAircraft(new Scanner(System.in)));
 
+        addAirport.addActionListener(e -> RepService.addAirport(new Scanner(System.in)));
+        editAirport.addActionListener(e -> RepService.editAirport(new Scanner(System.in)));
+        deleteAirport.addActionListener(e -> RepService.deleteAirport(new Scanner(System.in)));
+
+        addAirline.addActionListener(e -> RepService.addAirline(new Scanner(System.in)));
+
+        deleteAirline.addActionListener(e -> RepService.deleteAirline(new Scanner(System.in)));
+
+        editFlight.addActionListener(e -> RepService.editFlight(new Scanner(System.in)));
+
+        logout.addActionListener(e -> dispose());
         setVisible(true);
     }
 }
